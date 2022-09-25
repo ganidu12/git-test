541924fc691c4637541a103b420344563c20fa44
ganidu12 <114327660+ganidu12@users.noreply.github.com>
2022-09-25 11:26:52 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,21 @@
+a = "Hello out there."
+print(a)
+b = 'Hello again.'
+print(b)
+
+print (a+b)
+
+total = str(10 )
+greet =  'Hello'
+both = total + greet
+print(both)
+
+a = '10'
+b = '9'
+c = a + b
+print (c)
+
+
+#question 3
+
+print('test'+"\"+"test2") 
